370d0b2139001f03566fd3127901b2e66e3fb770
al3x4nd3r <alexanderstower@gmail.com>
2016-09-19 01:48:46 +0000

    problema de las dimensiones resuelto

--- www/js/aux.js
+++ www/js/aux.js
@@ -1,6 +1,6 @@
 var anchoDePantalla = window.screen.availWidth;
 var anchoDeTablero = 0.92 * anchoDePantalla;
-var anchoDeCelda = 0.18 * anchoDePantalla;
+var anchoDeCelda = 0.28 * anchoDePantalla;
 var espacioDeCelda = 0.04 * anchoDePantalla;
 
 function getPosTop(i, j){
