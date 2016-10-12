d7188bcd3cf10f327aa44582ffcf65525a13d2aa
al3x4nd3r <alexanderstower@gmail.com>
2016-10-12 22:07:40 +0000

    Ya se han conectado las vistas. Falta obligar a que el usuario elija una opción para no tener cosas vacías. Falta mejorar la interfaz de dimensiones y tablero. Las imagenes del tablero no están bien

--- platforms/browser/www/js/menu.js
+++ platforms/browser/www/js/menu.js
@@ -1,17 +1,33 @@
 
 $(document).ready(function(){
     
+    $(".area-sprite").on("click", resaltarAreasSprite);
+    $(".opcion-sprite").on("click", resaltarOpcionSprite);
     mostrarMenu();
     
 });
 
 
+diccionario_imagenes = { 1:"../img/cueva.jpg"
+                        ,2:"../img/petroglifo.png"
+                        ,3:"../img/amazonas.jpg"
+                        ,4:"../img/falcon.jpg"
+                        };
+
+var url_imagen_elegida = "";
+var dimension_x=3;
+var dimension_y=3
 
 function ocultarVistas(){
     $(".vista").css("display","none");
 }
 
 function mostrarMenu(){
+    ocultarVistas();
+    $("#vista-menu").css("display","block");
+}
+
+function mostrarDimensiones(){
     ocultarVistas();
     $("#vista-dimensiones").css("display","block");
 }
@@ -26,14 +42,11 @@ function jugarYa(){
     
     $("#tablero").remove();
     $("#fila").prepend('<div id="tablero"></div>');
-    
-    var url_imagen_elegida = $("#select-imagen").val();
-    var dimension_x = parseInt($("#select-dimension-x").val(),10);
-    var dimension_y = parseInt($("#select-dimension-y").val(),10);
-    
+    alert("HOLA");
     dimX = dimension_x;
     dimY = dimension_y;
     url_imagen = url_imagen_elegida;
+    alert(url_imagen_elegida);
     
     generarCeldas();
     prepararResponsive();
@@ -41,3 +54,45 @@ function jugarYa(){
     
     mostrarJuego();
 }
+
+function seleccionarImagen(numeroImagen){
+    
+    url_imagen_elegida = diccionario_imagenes[numeroImagen];
+}
+
+function seleccionarDimension(x,y){
+    dimension_x = parseInt(x,10);
+    dimension_y = parseInt(y,10);
+}
+
+function resaltarAreasSprite(event){
+    
+    $(".area-sprite").css({
+        "background-color":"initial",
+        "opacity": "initial",
+        "filter": "initial"
+    });
+    var areaTocada = $(event.target);
+    areaTocada.css({
+        "background-color":"white",
+        "opacity": "0.5",
+        "filter": "alpha(opacity=50)"
+    });
+    
+}
+
+function resaltarOpcionSprite(event){
+    
+    $(".opcion-sprite").css({
+        "background-color":"initial",
+        "opacity": "initial",
+        "filter": "initial"
+    });
+    var areaTocada = $(event.target);
+    areaTocada.css({
+        "background-color":"white",
+        "opacity": "0.5",
+        "filter": "alpha(opacity=50)"
+    });
+}
+
